ac33745cc7f402d0812bbba1ab5335abcac90ef5
will <ljclg_1516@foxmail.com>
2016-04-16 11:38:50 +0800

    update

--- BDMS_2016_spring/hw1/bootup.sh
+++ BDMS_2016_spring/hw1/bootup.sh
@@ -1,15 +1,15 @@
 
 #先格式化
-rm -rf ~/work/hdfs ~/work/hbase-zookeeper
-hdfs namenode -format
+#rm -rf ~/work/hdfs ~/work/hbase-zookeeper
+#hdfs namenode -format
 
 #再启动服务
 start-dfs.sh
 
 #放好数据
 hdfs dfsadmin -safemode leave
-hdfs dfs -mkdir hdfs://localhost:9000/hw1/
-hdfs dfs -put ~/work/hw1/tpch/* hdfs://localhost:9000/hw1/
-hdfs dfs -put ~/work/hw1/hw1-check/input/* hdfs://localhost:9000/hw1/
+hdfs dfs -mkdir /hw1
+hdfs dfs -put ./tpch/* /hw1
+hdfs dfs -put ./hw1-check/input/* /hw1
 echo '配置完成，是否关闭服务？'
 stop-dfs.sh
